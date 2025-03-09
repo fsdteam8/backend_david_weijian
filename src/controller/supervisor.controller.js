5cb5154a1f992435dc247168bc67c3102a0a261c
NurMohammad56 <milonsardar56@gmail.com>
2025-03-09 10:24:12 +0600

    add get all users functionality for supervisor

---
+++ src/controller/supervisor.controller.js
@@ -0,0 +1,23 @@
+import { Auth } from "../model/auth.model.js"
+
+// Get all users (Supervisor can view, but not update or delete)
+const getAllUsersForSupervisor = async (req, res) => {
+  try {
+    const users = await Auth.find().select("-password -refreshToken");
+
+    if (!users) {
+      return res.status(404).json({ status: false, message: 'No users found' });
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: 'Users fetched successfully for supervisor',
+      data: users
+    });
+  } catch (error) {
+    console.log("Error getting users from supervisor", error);
+    return res.status(500).json({ status: false, message: error.message });
+  }
+};
+
+export { getAllUsersForSupervisor };
