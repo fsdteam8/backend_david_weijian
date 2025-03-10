315b0d01fd352be5d175b5d5b52fb28fe7e08192
NurMohammad56 <milonsardar56@gmail.com>
2025-03-10 15:07:58 +0600

    add get all test centre functionality for user

--- src/route/testCentre.route.js
+++ src/route/testCentre.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {searchTestCentres} from "../controller/testCentre.controller.js"
+import {searchTestCentres, getTestCenterDetails} from "../controller/testCentre.controller.js"
 import {verifyJWT} from "../middleware/auth.middleware.js"
 
 const router = express.Router();
 
 router.get('/search', verifyJWT, searchTestCentres);
+router.get('/details/:id', verifyJWT, getTestCenterDetails);
 
 export default router
