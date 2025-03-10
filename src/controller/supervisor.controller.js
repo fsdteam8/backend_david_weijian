014070ad7a7782697ad555143c15cbc79db6c7d7
NurMohammad56 <milonsardar56@gmail.com>
2025-03-10 11:03:26 +0600

    add supervisor login functionality with routes and controller

--- src/controller/supervisor.controller.js
+++ src/controller/supervisor.controller.js
@@ -1,6 +1,37 @@
 import { Auth } from "../model/auth.model.js"
+import {generateAccessAndRefreshToken} from "../controller/auth.controller.js"
 
-// Get all users (Supervisor can view, but not update or delete)
+// Supervisor Login
+const supervisorLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await Auth.findOne({ email, who: "supervisor" }); 
+    if (!user) {
+      return res.status(404).json({status: false, message: "Supervisor not found" });
+    }
+
+    const isPasswordValid = await user.isPasswordValid(password);
+    if (!isPasswordValid) {
+      return res.status(401).json({status: false, message: "Invalid credentials" });
+    }
+
+    const { accessToken } = await generateAccessAndRefreshToken(user._id);
+
+    await user.save();
+
+    res.status(200).json({
+      status: true,
+      message: "Supervisor login successful",
+      accessToken,
+    });
+  } catch (error) {
+    console.log("Error while login as supervisor", error);
+    res.status(500).json({status: false, message: "Server error", error: error.message });
+  }
+};
+
+// <<<<<<<<<<<<<<<<<<<< ALL USERS >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 const getAllUsersForSupervisor = async (_, res) => {
   try {
     const users = await Auth.find().select("-password -refreshToken");
@@ -20,4 +51,4 @@ const getAllUsersForSupervisor = async (_, res) => {
   }
 };
 
-export { getAllUsersForSupervisor };
+export { getAllUsersForSupervisor, supervisorLogin };
