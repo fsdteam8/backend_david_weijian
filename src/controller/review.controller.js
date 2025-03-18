5071dd5067b904f1a3cb87a7bb5f39e7137e0523
NavidulHoque <hnavidul@gmail.com>
2025-03-18 09:46:28 +0600

    feat: add getARoute endpoint to retrieve route details; refactor createReview to return populated review

--- src/controller/review.controller.js
+++ src/controller/review.controller.js
@@ -10,19 +10,24 @@ const createReview = async (req, res) => {
       reviewText,
     });
 
-    if (!review){
-        return res.status(400).json({ status: false, message: "Please fill up properly" });
+    if (!review) {
+      return res.status(400).json({ status: false, message: "Please fill up properly" });
     }
 
     await review.save();
-    return res.status(201).json({ status: true, message: "Review created successfully", data: review });
 
-  } catch (error) {
+    const populatedReview = await Review.findById(review._id)
+      .populate("routeId");
+
+    return res.status(201).json({ status: true, message: "Review created successfully", data: populatedReview });
+  }
+
+  catch (error) {
     console.log("Error creating review", error);
     return res.status(500).json({ status: false, message: error.message });
   }
 };
 
 export {
-    createReview
+  createReview
 }
