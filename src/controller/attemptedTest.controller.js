ff0741d67c62b64b7689b27337db5663d83b3f43
NavidulHoque <hnavidul@gmail.com>
2025-03-25 14:11:28 +0600

    feat: implement createAttemptedTest controller and update routes

--- src/controller/attemptedTest.controller.js
+++ src/controller/attemptedTest.controller.js
@@ -1,1 +1,27 @@
+import { AttemptedTest } from "../model/attemptedTest.model.js";
 
+export const createAttemptedTest = async (req, res) => {
+
+    const { passRate, testCentreId, routeId, isCompleted } = req.body
+    const userId = req.user.id;
+
+    try {
+        const newAttemptedTest = new AttemptedTest({
+            passRate,
+            testCentreId,
+            userId,
+            routeId,
+            isCompleted
+        });
+        await newAttemptedTest.save();
+        res.status(201).json({
+            status: true,
+            message: "Attempted test created successfully",
+            data: newAttemptedTest,
+        });
+    }
+
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
