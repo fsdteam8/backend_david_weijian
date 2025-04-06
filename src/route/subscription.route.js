ddf5fea8b7edf5f9ce2fd4d58e20e5d8ca3e30f1
NurMohammad56 <milonsardar56@gmail.com>
2025-04-06 15:14:01 +0600

    feat: add JWT verification to subscription and transaction routes

--- src/route/subscription.route.js
+++ src/route/subscription.route.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { createSubscription, getAllSubscriptions, getSubscription, updateSubscription, deleteSubscription } from '../controller/subscription.controller.js';
-
+import { verifyJWT } from '../middleware/auth.middleware.js';
 const router = express.Router();
 
-router.post('/create-subscription', createSubscription);
-router.get('/', getAllSubscriptions);
-router.get('/:id', getSubscription);
-router.patch('/update/:id', updateSubscription);
-router.delete('/delete/:id', deleteSubscription);
+router.post('/create-subscription', verifyJWT, createSubscription);
+router.get('/', verifyJWT, getAllSubscriptions);
+router.get('/:id', verifyJWT, getSubscription);
+router.patch('/update/:id',verifyJWT, updateSubscription);
+router.delete('/delete/:id',verifyJWT, deleteSubscription);
 
 export default router;
